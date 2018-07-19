e4fe730e2e83c54a41f7023cf13d9471e1234fbd
OUO <1511876221@qq.com>
2018-07-19 10:16:23 +0800

    返回Json

--- src/main/java/cn/springmvc/controller/LoginController.java
+++ src/main/java/cn/springmvc/controller/LoginController.java
@@ -1,9 +1,11 @@
 package cn.springmvc.controller;
 
 
+import cn.springmvc.base.JsonBean;
 import org.springframework.stereotype.Controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/")
@@ -12,5 +14,19 @@ public class LoginController {
     @RequestMapping("loginView")
     public String gotoLoginView(){
       return "loginView";
+    }
+
+
+    /**
+     * 返回json类型数据
+     *
+     * @return
+     */
+    @RequestMapping("getJson")
+    @ResponseBody
+    public JsonBean getJsonData(){
+        JsonBean jsonBean = new JsonBean();
+        jsonBean.put("jsonData","lalalalal");
+        return jsonBean;
     }
 }
